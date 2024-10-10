ae21c911802368b9f1f3550b319cddad695edc57
Leonin04 <davidbacasposadas@gmail.com>
2024-10-10 15:56:04 +0700

    Prototype of singleplayer

--- UI/iq180/src/Menu.js
+++ UI/iq180/src/Menu.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Howtoplay from './Howtoplay';
 import RandomLetter from './Effects';
-import './App.css';
+import './Menu.css';
 
 
 
