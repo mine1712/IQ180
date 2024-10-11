b4257eb76cc99bbe0be740460bd77e1ad26c8149
spicakitty <178306909+spicakitty@users.noreply.github.com>
2024-10-11 13:16:43 +0700

    Play Area Format added

--- iq180-client/src/pages/Multiplayer.jsx
+++ iq180-client/src/pages/Multiplayer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-// import '../css/Multiplayer.css';
+import '../css/Multiplayer.css';
 import io from 'socket.io-client';
 
 const server = io.connect('http://localhost:5172');
@@ -10,46 +10,85 @@ function Multiplayer () {
             <h1>Target = XX</h1>
             <p>Time remaining = XX</p>
             <p>It is your turn</p>
-            <div>
-                {[...Array(5).keys()].map((index) => (
-                    <span
-                        key={'playSlot'+(index+1)}
-                        // disabled={isTimeUp}
-                        // draggable
-                        // onDragStart={() => handleNumberDragStart(index)}
-                        // onDrop={() => handleNumberDrop(index)}
-                        // onDragOver={(e) => e.preventDefault()}
-                        // id = "span_num_ops"
-                    >
-                        {'playSlot'+(index+1)}
-                    </span>
-                ))}
-            </div>
-            <div>
-                {[1,3,5,7,9].map((number, index) => (
-                    <span
-                        key={'numberBank'+(index+1)}
-                        // disabled={isTimeUp}
-                        // draggable
-                        // onDragStart={() => handleNumberDragStart(index)}
-                        // onDrop={() => handleNumberDrop(index)}
-                        // onDragOver={(e) => e.preventDefault()}
-                        // id = "span_num_ops"
-                    >
-                        {'numberBank'+(index+1)+'='+(number)}
-                    </span>
-                ))}
-            </div>
-            <div>
-                {['+','-','x','÷'].map((symbol, index) => (
-                    <span
-                        key={'operatorBank'+(index+1)}
-                    >
-                        {'operatorBank'+(index+1)+'='+(symbol)}
-                    </span>
+            <div style={{textAlign:'center'}}>
+                <div>
+                    {[...Array(5).keys()].map((index) => {
+                        if (index==4) {
+                            return (
+                                <div id="playSlotBoxArea">
+                                    <span
+                                        key={'numberPlaySlot'+(index+1)}
+                                        id='numberPlaySlotBox'
+                                        // disabled={isTimeUp}
+                                        // draggable
+                                        // onDragStart={() => handleNumberDragStart(index)}
+                                        // onDrop={() => handleNumberDrop(index)}
+                                        // onDragOver={(e) => e.preventDefault()}
+                                        // id = "span_num_ops"
+                                    >
+                                        1
+                                    </span>
+                                </div>
+                            )
+                        } else return (
+                            <div id="playSlotBoxArea">
+                                <span
+                                    key={'numberPlaySlot'+(index+1)}
+                                    id='numberPlaySlotBox'
+                                    // disabled={isTimeUp}
+                                    // draggable
+                                    // onDragStart={() => handleNumberDragStart(index)}
+                                    // onDrop={() => handleNumberDrop(index)}
+                                    // onDragOver={(e) => e.preventDefault()}
+                                    // id = "span_num_ops"
+                                >
+                                    1
+                                </span>
+                                <span
+                                    key={'numberPlaySlot'+(index+1)}
+                                    id='operatorPlaySlotBox'
+                                    // disabled={isTimeUp}
+                                    // draggable
+                                    // onDragStart={() => handleNumberDragStart(index)}
+                                    // onDrop={() => handleNumberDrop(index)}
+                                    // onDragOver={(e) => e.preventDefault()}
+                                    // id = "span_num_ops"
+                                >
+                                    +
+                                </span>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div>
+                    {[1,3,5,7,9].map((number, index) => (
+                        <span
+                            key={'numberBank'+(index+1)}
+                            id = 'numberBankBox'
+                            // disabled={isTimeUp}
+                            // draggable
+                            // onDragStart={() => handleNumberDragStart(index)}
+                            // onDrop={() => handleNumberDrop(index)}
+                            // onDragOver={(e) => e.preventDefault()}
+                            // id = "span_num_ops"
+                        >
+                            {number}
+                        </span>
+                    ))}
+                </div>
+                <div>
+                    {['+','-','x','÷'].map((symbol, index) => (
+                        <span
+                            key={'operatorBank'+(index+1)}
+                            id = 'operatorBankBox'
+                        >
+                            {symbol}
+                        </span>
 
-                ))}
+                    ))}
+                </div>
             </div>
+            
         </div>
     )
 }
