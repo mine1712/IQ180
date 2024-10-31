78684d2d81209593152299366135147512509d9b
mattananta <58379005+mattananta@users.noreply.github.com>
2024-10-31 16:32:47 +0700

    Refactor code to center button container and add return button on Multiplayer page

--- iq180-client/src/pages/Multiplayer.jsx
+++ iq180-client/src/pages/Multiplayer.jsx
@@ -503,15 +503,57 @@ function Multiplayer ({goToPage}) {
                         </ul>
                     </nav>
                     <div id="divider"></div>
-                    {targetResult!==null && (
-                        <h1>Target = {targetResult}</h1>
-                    )}
-                    {timeLeft!=null && (
-                        <p>Time remaining = {timeLeft}</p>
-                    )}
-                    {attemptsLeft!== null && (
-                        <p>Attempts left = {attemptsLeft}</p>
-                    )}
+                    <div style={{display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        padding: '10px',
+                        }}> 
+                        {targetResult!==null && (
+                            <div style={{fontSize: '18px',
+                                fontWeight: '600', margin:'10px'}}><>{targetResult}</></div>
+                        )}
+                        {timeLeft!=null && (
+                            <div id ="countdown"style ={{position: 'relative',
+                                height: '40px',
+                                width: '40px',
+                                textAlign: 'center'}}>
+                                <div style={{color: 'black',
+                                    display: 'inline-block',
+                                    lineHeight: '40px',}} >{timeLeft}</div>
+                                <svg style={{position: 'absolute',
+                                    top: '0',
+                                    right: '0',
+                                    width: '40px',
+                                    height: '40px',
+                                    transform: 'rotateY(-180deg) rotateZ(-90deg)'}}>
+                                    <circle r="18" cx="20" cy="20" style={{strokeDasharray: '113px',
+                                        strokeDashoffset: '0px',
+                                        strokeLinecap: 'round',
+                                        strokeWidth: '2px',
+                                        stroke: 'black',
+                                        fill: 'none',
+                                        animation: timeLeft !== 0 ? 'countdown 60s linear infinite forwards' : 'none'}}></circle>
+                                </svg>
+                            </div>
+                        )}
+                        {attemptsLeft!== null && (
+                            <div style={{ fontSize: '18px', margin: '10px' }}>
+                                {Array.from({ length: attemptsLeft }).map((_, index) => (
+                                    <svg key={index} fill="#000000" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 471.701 471.701">
+                                        <g>
+                                            <path d="M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1
+                                            c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3
+                                            l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4
+                                            C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3
+                                            s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4
+                                            c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3
+                                            C444.801,187.101,434.001,213.101,414.401,232.701z"/>
+                                        </g>
+                                    </svg>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                     {/* <button onClick={() => {
                         setTimeLeft(roundLength);
                     }}>Reset Timer</button>
@@ -561,10 +603,14 @@ function Multiplayer ({goToPage}) {
                     {!isYourTurn && (
                         <h1 style={{textAlign:'center'}}>Please wait until it's your turn!</h1>
                     )}
-                    <button onClick={()=>{
-                        server.emit('exitRoom');
-                        goToPage("Menu");
-                    }}>Return to Menu</button>
+                    <div className="center-button-container">
+                        <button className="return" onClick={()=>{
+                            server.emit('exitRoom');
+                            goToPage("Menu");
+                        }} style={{border: 'none', background: 'none',backgroundColor:'#f9f9f9', border:'1px solid 1px solid transparent', borderRadius:'8px', padding:'5px', margin:'5px'}}><svg width="32px" height="32px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                        <path fill="#000000" fill-rule="evenodd" d="M4.297105,3.29289 L0.59,7 L4.297105,10.7071 C4.687635,11.0976 5.320795,11.0976 5.711315,10.7071 C6.101845,10.3166 6.101845,9.68342 5.711315,9.29289 L4.418425,8 L11.504215,8 C12.332615,8 13.004215,8.67157 13.004215,9.5 C13.004215,10.3284 12.332615,11 11.504215,11 L10.004215,11 C9.451935,11 9.004215,11.4477 9.004215,12 C9.004215,12.5523 9.451935,13 10.004215,13 L11.504215,13 C13.437215,13 15.004215,11.433 15.004215,9.5 C15.004215,7.567 13.437215,6 11.504215,6 L4.418425,6 L5.711315,4.70711 C6.101845,4.31658 6.101845,3.68342 5.711315,3.29289 C5.320795,2.90237 4.687635,2.90237 4.297105,3.29289 Z"/>
+                      </svg></button>
+                    </div>
                 </div>
             )}
         </div>     
